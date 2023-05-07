8a31370d98e0f4a14467532f85ebd2ee4d679dc9
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-05-07 18:07:41 +0100

    first commit

--- aurous79/utils/login_required.py
+++ aurous79/utils/login_required.py
@@ -2,13 +2,12 @@ from flask import flash, session, redirect, url_for
 from functools import wraps
 
 
-def login_required(f):
+def login_required(func):
     """Redirect user to login page if not logged in"""
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if "logged_in" in session:
-            return f(*args, **kwargs)
-        else:
-            flash("You need to login first.")
-            return redirect(url_for("login"))
-    return wrap
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if session.get('logged_in') is None:
+            flash('You need to log in first')
+            return redirect(url_for('login'))
+        return func(*args, **kwargs)
+    return decorated_function
