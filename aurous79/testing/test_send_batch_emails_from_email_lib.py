2008dde7f2dfe124f0b905ddd78c563164e7499a
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-05-09 04:56:33 +0100

    test to check mass emails are sent correctly

--- aurous79/testing/test_send_batch_emails_from_email_lib.py
+++ aurous79/testing/test_send_batch_emails_from_email_lib.py
@@ -4,6 +4,7 @@ from aurous79 import app, init_mail
 from aurous79.extension import SessionLocal
 from aurous79.testing.client import client
 from aurous79.models import EmailLibrary
+from aurous79.utils.add_to_email_library import add_to_email_library
 from aurous79.utils.create_feedback import create_feedback
 from aurous79.utils.mass_email_from_email_library import send_batch_emails_from_email_lib
 
@@ -13,31 +14,30 @@ def test_send_batch_emailLibrary_emails():
     session: SessionLocal = SessionLocal()
     # create 3x feedback forms
     customer1: EmailLibrary = EmailLibrary(
-        name="John Doe",
-        email="john@email.com",
-        timestamp=datetime.now(),
+        customer_name="John Doe",
+        customer_email="john@email.com",
     )
-    customer1_feedback: EmailLibrary = create_feedback(
-        name=customer1.name,
-        email=customer1.email,
+    new_customer1: EmailLibrary = add_to_email_library(
+        name=customer1.customer_name,
+        email=customer1.customer_email,
     )
 
     customer2: EmailLibrary = EmailLibrary(
-        name="Jane Doe",
-        email="jane@email.com",
+        customer_name="Jane Doe",
+        customer_email="jane@email.com",
     )
-    customer2_feedback: EmailLibrary = create_feedback(
-        name=customer2.name,
-        email=customer2.email,
+    new_customer2: EmailLibrary = add_to_email_library(
+        name=customer2.customer_name,
+        email=customer2.customer_email,
     )
 
     customer3: EmailLibrary = EmailLibrary(
-        name="Max Jones",
-        email="max@email.com",
+        customer_name="Max Jones",
+        customer_email="max@email.com",
     )
-    customer3_feedback: EmailLibrary = create_feedback(
-        name=customer3.name,
-        email=customer3.email,
+    new_customer3: EmailLibrary = add_to_email_library(
+        name=customer3.customer_name,
+        email=customer3.customer_email,
     )
 
     with app.app_context():
@@ -48,20 +48,20 @@ def test_send_batch_emailLibrary_emails():
             sent_email: bool = send_batch_emails_from_email_lib(email_subject, email_content)
             
             # check if emails were sent
-            # assert sent_email is True
+            assert sent_email is True
 
-#             # check if outbox object exists
-#             assert len(outbox) == 1
-#             assert outbox[0].subject == email_subject
-#             assert outbox[0].recipients == [customer1.email, customer2.email, customer3.email]
-#             assert email_content in outbox[0].body
-#             assert outbox[0].body == f"{email_content}\n\n"
+            # check if outbox object exists
+            assert len(outbox) == 1
+            assert outbox[0].subject == email_subject
+            assert outbox[0].recipients == [customer1.customer_email, customer2.customer_email, customer3.customer_email]
+            assert email_content in outbox[0].body
+            assert outbox[0].body == f"{email_content}\n\n"
         
     
     # remove db data
-    session.delete(customer1_feedback)
-    session.delete(customer2_feedback)
-    session.delete(customer3_feedback)
+    session.delete(new_customer1)
+    session.delete(new_customer2)
+    session.delete(new_customer3)
     session.commit()
 
     with app.app_context():
