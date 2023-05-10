17c30c499c756f50184e6902f2ae38f3408a085c
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-05-10 23:31:32 +0100

    test cases for male report & other test cases

--- aurous79/testing/test_feedback_report.py
+++ aurous79/testing/test_feedback_report.py
@@ -48,6 +48,8 @@ def test_get_male_report(client):
         # check if db object is same as feedback object
         assert customer.sex == get_feedback.sex
         assert customer.first_visit == get_feedback.first_visit
+        assert customer.return_visit == get_feedback.return_visit
+        assert customer.shisha == get_feedback.shisha
         
         # check male results
         male_result: FeedbackForm = get_male_report()
@@ -62,9 +64,16 @@ def test_get_male_report(client):
         assert all(data.first_visit == "yes" for data in first_visit_results)
 
         # check return_visit results
+        return_visit_results: FeedbackForm = get_return_visit_report()
+        assert return_visit_results[0].return_visit == "yes"
+        assert return_visit_results[0].return_visit == get_feedback.return_visit
+        assert all(data.return_visit == "yes" for data in return_visit_results)
 
         # check get shisha results
-
+        shisha_results: FeedbackForm = get_shisha_report()
+        assert shisha_results[0].shisha == "yes"
+        assert shisha_results[0].shisha == get_feedback.shisha
+        assert all(data.shisha == "yes" for data in shisha_results)
     
     # remove db data
     session.delete(customer_feedback)
