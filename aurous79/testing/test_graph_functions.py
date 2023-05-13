c4b0421eeabd62938267bacd57f6ede60697a10b
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-05-13 21:41:05 +0100

    test case for first visit graph functions

--- aurous79/testing/test_graph_functions.py
+++ aurous79/testing/test_graph_functions.py
@@ -112,6 +112,7 @@ def test_male_axis(
     email,
     timestamp,
 ):
+    """Test to find all male entries from db using male_x & male_y functions"""
     session: SessionLocal = SessionLocal()
     # create feedback object
     customer: FeedbackForm = FeedbackForm(
@@ -202,6 +203,7 @@ def test_female_axis(
     email,
     timestamp,
 ):
+    """Test to find all female entries from db using female_x & female_y functions"""
     session: SessionLocal = SessionLocal()
     # create feedback object
     customer: FeedbackForm = FeedbackForm(
@@ -254,7 +256,7 @@ def test_female_axis(
         # check if db object is the same as feedback object
         assert get_1_feedback.sex == "female"
         assert customer.sex == get_1_feedback.sex
-        # check if num of males macth X & Y axis function
+        # check if num of females macth X & Y axis function
         get_all_feedback: List[FeedbackForm] = FeedbackForm.query.all()
         assert all(data.sex == sex for data in get_all_feedback)
 
@@ -263,6 +265,97 @@ def test_female_axis(
 
         assert female_y() == 2
         assert female_y() == len(get_all_feedback)
+
+    session.delete(customer_feedback_1)
+    session.delete(customer_feedback_2)
+    session.commit()
+
+    with app.app_context():
+        get_1_feedback: FeedbackForm = FeedbackForm.query.first()
+        assert get_1_feedback == None
+
+
+@pytest.mark.parametrize(
+    "name,age,sex,first_visit,return_visit,clean,service,speed,food_quality,shisha,comment,email,timestamp",
+    male_results,
+)
+def test_first_visit_axis(
+    name,
+    age,
+    sex,
+    first_visit,
+    return_visit,
+    clean,
+    service,
+    speed,
+    food_quality,
+    shisha,
+    comment,
+    email,
+    timestamp,
+):
+    """Test to find all first_visit entries from db using X & Y functions"""
+    session: SessionLocal = SessionLocal()
+    # create feedback object
+    customer: FeedbackForm = FeedbackForm(
+        name=name,
+        age=age,
+        sex=sex,
+        first_visit=first_visit,
+        return_visit=return_visit,
+        clean=clean,
+        service=service,
+        speed=speed,
+        food_quality=food_quality,
+        shisha=shisha,
+        comment=comment,
+        email=email,
+        timestamp=timestamp,
+    )
+    # store feedback object to db
+    customer_feedback_1: FeedbackForm = create_feedback(
+        name=customer.name,
+        age=customer.age,
+        sex=customer.sex,
+        first_visit=customer.first_visit,
+        return_visit=customer.return_visit,
+        clean=customer.clean,
+        service=customer.service,
+        speed=customer.speed,
+        food_quality=customer.food_quality,
+        shisha=customer.shisha,
+        comment=customer.comment,
+        email=customer.email,
+    )
+    customer_feedback_2: FeedbackForm = create_feedback(
+        name=customer.name,
+        age=customer.age,
+        sex=customer.sex,
+        first_visit=customer.first_visit,
+        return_visit=customer.return_visit,
+        clean=customer.clean,
+        service=customer.service,
+        speed=customer.speed,
+        food_quality=customer.food_quality,
+        shisha=customer.shisha,
+        comment=customer.comment,
+        email=customer.email,
+    )
+    with app.app_context():
+        # get 1st feedback object from db
+        get_1_feedback: FeedbackForm = FeedbackForm.query.first()
+        # check if db object is the same as feedback object
+        assert get_1_feedback.return_visit == "yes"
+        assert customer.return_visit == get_1_feedback.return_visit
+        # check if num of return_visit macth X & Y axis function
+        get_all_feedback: List[FeedbackForm] = FeedbackForm.query.all()
+        assert all(data.return_visit == return_visit for data in get_all_feedback)
+
+        assert return_visit_x() == 2
+        assert return_visit_x() == len(get_all_feedback)
+
+        assert return_visit_y() == 2
+        assert return_visit_y() == len(get_all_feedback)
 
     session.delete(customer_feedback_1)
     session.delete(customer_feedback_2)
