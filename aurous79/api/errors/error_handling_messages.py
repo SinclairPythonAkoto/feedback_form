ad33254808aa831fecff5427ebfe807cb6572f45
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-05-18 21:34:46 +0100

    removed error handlers that wasn't working (can't have multiple errors with the same status code)

--- aurous79/api/errors/error_handling_messages.py
+++ aurous79/api/errors/error_handling_messages.py
@@ -13,18 +13,8 @@ def handle_not_found_error(error):
 def handle_internal_server_error(error):
     return jsonify({'error': 'Internal Server Error'}), 500
 
-# create & define custom error handlers
-@app.errorhandler(404)
-def handle_no_feeback_form_error(error):
-    """Error message to handle no feedback form in db"""
-    return jsonify({"error": "Feedback form not found. The feedback form is empty."})
 
-@app.errorhandler(404)
-def handle_no_email_error(error):
-    """Error message to handle no email in email library db"""
-    return jsonify({"error": "No email found. The Aurous email library is empty."})
-
+# Define a custom error handler for 403 Unauthorized Access errors
 @app.errorhandler(403)
 def handle_unauthorized_access_error(error):
-    """Error message to handle unauthorized access to routes"""
-    return jsonify({"error": "Unauthorized access."})
+    return jsonify({'error': 'Unauthorized access'}), 403
--- aurous79/api/services/public/public_api_service.py
+++ aurous79/api/services/public/public_api_service.py
@@ -1,10 +1,19 @@
 import os
 from typing import List, Dict
+from flask import request
 from aurous79 import app
 from aurous79.extension import SessionLocal
 from aurous79.models import FeedbackForm
-from aurous79.api.errors.error_handling_messages import handle_not_found_error
+from aurous79.api.errors.error_handling_messages import handle_not_found_error, handle_unauthorized_access_error
 from flask import jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# define list of API keys
+# private key is not needed for this section of API
+VALID_API_KEYS = [os.environ["PUBLIC_API_KEY"], os.environ["PRIVATE_API_KEY"]]
 
 
 @app.route("/api/v1/service/public")
@@ -20,9 +29,34 @@ def public_feedback_api():
     session: SessionLocal = SessionLocal()
     feedback_results: List[FeedbackForm] = session.query(FeedbackForm).all()
     if len(feedback_results) == 0:
-        error_message: Dict[str] = {"error": "Message board is empty."}
         return handle_not_found_error(404)
-    data: Dict[List[FeedbackForm]] = {
-        "Feedback Results": [val.to_dict() for val in feedback_results]
+    data: Dict[List[FeedbackForm]] ={
+        "Feedback form results": [
+            {
+                "Gender": val.sex,
+                "Fisrt Visit": val.first_visit,
+                "Return Visit": val.return_visit,
+                "Tried Shisha": val.shisha,
+                "Speed": val.speed,
+                "Service": val.service,
+                "Food Quality": val.food_quality,
+            }
+            for val in feedback_results
+        ]
     }
     return jsonify(data), 200
+
+
+
+# get feedback form via ID using public API key
+@app.route('/api/v1/service/feedback/<int:id>', methods=['GET'])
+def get_feedback_via_id(id: int):
+    # Check if the API key is valid
+    if 'X-Api-Key' not in request.headers or request.headers['X-Api-Key'] not in VALID_API_KEYS:
+        # create no api key found 
+        return handle_unauthorized_access_error(403)
+    
+
+    # Return some data
+    data = {'name': 'Alice', 'email': 'alice@example.com'}
+    return jsonify(data)
